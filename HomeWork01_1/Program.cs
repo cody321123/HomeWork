2e017a5bd181db979c1a80bfc63a4071f6deafae
«Aleksandr» <navalniu202@gmail.com>
2022-06-18 16:43:49 +0300

    Homework1

---
+++ HomeWork01_1/Program.cs
@@ -0,0 +1,16 @@
+﻿int a, b;
+
+Console.WriteLine("Введите 2 числа");
+
+a = Convert.ToInt32(Console.ReadLine());
+
+b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+          Console.WriteLine("Наибольшее число " + a);
+}
+else
+{
+          Console.WriteLine("наибольшее число " + b);
+}
