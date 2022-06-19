d6c0e1f478883ecda6d8f87132d6262e7bbf5438
«Aleksandr» <navalniu202@gmail.com>
2022-06-19 04:12:09 +0300

    HomeWork4

---
+++ HomeWork01_4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число:");
+int endNum = Convert.ToInt32(Console.ReadLine());
+int num = 1;
+
+while (num < endNum)
+{
+    num++;
+    if (num % 2 == 0) Console.Write(num + " ");
+}
