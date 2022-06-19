56367e5bc2526a2ef2905ed4cafb18f983253f96
«Aleksandr» <navalniu202@gmail.com>
2022-06-19 03:30:16 +0300

    HomeWork2.1

--- HomeWork01_2/Program.cs
+++ HomeWork01_2/Program.cs
@@ -5,13 +5,19 @@ int b = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите третье число: ");
 int c = Convert.ToInt32(Console.ReadLine());
  
-if (a > b & a > c)
+if (a == b & a ==c)
+{
+        Console.WriteLine("Числа равны"); 
+}
+else
+
+if (a >= b & a >= c)
 {
           Console.WriteLine("Максимальное число " + a);
 }
 else
 
-          if (b > a & b > c)
+          if (b >= a & b >= c)
           {
                     Console.WriteLine("Максимальное число " + b);
           }
