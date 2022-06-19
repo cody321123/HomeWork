ff5c2df096bc36c72b3cb03151d6b829c4952722
«Aleksandr» <navalniu202@gmail.com>
2022-06-19 03:24:19 +0300

    HomeWork2

---
+++ HomeWork01_2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+ 
+if (a > b & a > c)
+{
+          Console.WriteLine("Максимальное число " + a);
+}
+else
+
+          if (b > a & b > c)
+          {
+                    Console.WriteLine("Максимальное число " + b);
+          }
+    
+else
+          Console.WriteLine("Максимальное чиcло " + c);
